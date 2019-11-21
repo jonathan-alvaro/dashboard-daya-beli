3de761bb2966961ec6a7da2d0f53a0a585d0a550
Jonathan Alvaro <jonalvarot@gmail.com>
2019-11-21 19:53:49 +0700

    Removed QoQ graph

--- new_app.py
+++ new_app.py
@@ -49,41 +49,17 @@ app.layout = html.Div([
     [Input('menu-dropdown', 'value')]
 )
 def refresh_content(selected_menu):
-    # Load QoQ data
-    qoq_X, qoq_y, qoq_inflation, qoq_timestamps = load_qoq_data(
-        os.path.join(data_dir, 'all_merged.csv')
-    )
 
     # Load YoY data
     yoy_X, yoy_y, yoy_inflation, yoy_timestamps = load_yoy_data(
         os.path.join(data_dir, 'yoy_non_food.csv')
     )
 
-    # # Forecast future data
-    qoq_preds = predict_qoq(model_dir, qoq_X)
+    # Forecast future data
     yoy_preds = predict_yoy(model_dir, yoy_X)
 
     if selected_menu == 'forecast':
         divs = []
-
-        qoq_latest_pred = qoq_preds[-1]
-        qoq_latest_data = qoq_y.tail(1).values[0]
-        qoq_latest_timestamp = qoq_timestamps.tail(1).values[0]
-        qoq_change = qoq_latest_pred - qoq_latest_data
-
-        qoq_new_year = int(qoq_latest_timestamp[:4])
-        qoq_new_quarter = int(qoq_latest_timestamp[-1]) + 1
-        if qoq_new_quarter > 4:
-            qoq_new_year += 1
-            qoq_new_quarter %= 4
-        qoq_new_timestamp = f'{qoq_new_year}-Q{qoq_new_quarter}'
-
-        if qoq_change >= 0:
-            qoq_text = f'Naik {round(qoq_change, 2)}%'
-            qoq_text_color = 'green'
-        else:
-            qoq_text = f'Turun {-round(qoq_change, 2)}%'
-            qoq_text_color = 'red'
 
         yoy_latest_pred = yoy_preds[-1]
         yoy_latest_data = yoy_y.tail(1).values[0]
@@ -104,82 +80,6 @@ def refresh_content(selected_menu):
             yoy_text = f'Turun {-round(yoy_change, 2)}%'
             yoy_text_color = 'red'
     
-        divs.append(html.Div(
-            [
-                html.Div(
-                    [
-                        html.P(
-                            f"Prediksi Daya Beli QoQ {qoq_new_timestamp}",
-                            style={
-                                'align-self':'center',
-                                'font-size':'1em'
-                            }
-                        ),
-                        html.P(
-                            str(round(qoq_preds[-1], 2)) + "%",
-                            style={
-                                'align-self':'center',
-                                'font-size':'5em',
-                                'margin':0
-                            }
-                        )
-                    ],
-                    style={
-                        'display':'flex',
-                        'align-content':'center',
-                        'flex-direction':'column',
-                        'align-self': 'right',
-                        'margin-right':'2.5%'
-                    }
-                ),
-                html.Div(
-                    [
-                        html.P(
-                            "Perubahan Daya Beli QoQ",
-                            style={
-                                'align-self':'center',
-                                'font-size':'1em'
-                            }
-                        ),
-                        html.P(
-                            str(round(qoq_change, 2)) + "%",
-                            style={
-                                'align-self':'center',
-                                'font-size':'5em',
-                                'margin':0
-                            }
-                        )
-                    ],
-                    style={
-                        'display':'flex',
-                        'align-content':'center',
-                        'flex-direction':'column',
-                        'color': qoq_text_color,
-                        'align-self': 'right',
-                        'margin-right':'1%'
-                    }
-                )
-            ],
-            style={
-                'display': 'flex',
-                'align-content':'center',
-                'flex-direction':'row',
-                'justify-content':'flex-end'
-            }
-        ))
-
-        divs.append(html.Div(
-            [
-                plot_prediction_graph_qoq(
-                    qoq_timestamps, qoq_y, qoq_preds, qoq_inflation, 'Daya Beli QoQ'
-                )
-
-            ],
-            style={
-                'width':'100%'
-            }
-        ))
-
         divs.append(html.Div(
             [
                 html.Div(
