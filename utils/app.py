9bed72f413722c052bd98dfa907512ac6e3848ed
Jonathan Alvaro <jonalvarot@gmail.com>
2019-11-22 07:18:48 +0700

    Remove quarter comparison graph

--- utils/app.py
+++ utils/app.py
@@ -8,6 +8,8 @@ def create_non_food_variable_graphs(predictors, targets, timestamps):
     graphs = []
     
     for col in predictors:
+        if col == 'Quarter':
+            continue
         graph = dcc.Graph(
             figure=go.Figure(
                 data = [
@@ -46,17 +48,26 @@ def create_non_food_variable_graphs(predictors, targets, timestamps):
                         'x': 0.5
                     },
                     xaxis={
-                        'showgrid':False
+                        'showgrid':False,
+                        'showline':True,
+                        'linecolor':'#A9A9A9'
                     },
                     yaxis={
                         'title':'Change Daya Beli (%)',
-                        'showgrid':False
+                        'showgrid':False,
+                        'gridcolor':'#A9A9A9',
+                        'zerolinecolor':'#A9A9A9',
+                        'showgrid':True,
+                        'showline':True,
+                        'linecolor': '#A9A9A9'
+
                     },
                     yaxis2={
                         'title':f'Change {col} (%)',
                         'side':'right',
                         'overlaying':'y',
-                        'showgrid':False
+                        'showgrid':False,
+                        'zerolinecolor':'#A9A9A9'
                     },font={
                         'color': '#ccdbdc'
                     },
@@ -69,86 +80,6 @@ def create_non_food_variable_graphs(predictors, targets, timestamps):
         graphs.append(graph)
     
     return graphs
-
-
-def plot_prediction_graph_qoq(timestamps, data, prediction, inflation, title):
-
-    pred_quarter = int(timestamps.tail(1).values[0][-1]) + 1
-    pred_year = int(timestamps.tail(1).values[0][:4])
-
-    if pred_quarter > 4:
-        pred_quarter -= 4
-        pred_year += 1
-
-    timestamps[len(timestamps)] = f'{pred_year}-Q{pred_quarter}'
-    timestamps = timestamps.apply(lambda x : x[2:])
-    timestamps = timestamps.reset_index(drop=True)
-
-    prediction = pd.Series(prediction)
-    data = pd.Series(data)
-
-    prediction = np.append(data.tail(1).values, prediction.tail(1).values[0])
-    prediction = pd.Series(prediction)
-
-    if prediction.tail(1).values[0] < data.tail(1).values[0]:
-        prediction_line_color = 'red'
-    else:
-        prediction_line_color = 'green'
-
-    return dcc.Graph(
-        figure=go.Figure(
-            data = [
-                go.Line(
-                    x=timestamps, 
-                    y=data,
-                    name='Daya Beli QoQ',
-                    mode='lines+markers',
-                    line={
-                        'color':'#ccdbdc'
-                    }
-                ),
-                go.Line(
-                    x=timestamps.tail(2),
-                    y=prediction,
-                    name='Daya Beli QoQ Prediksi',
-                    mode='lines+markers',
-                    line={
-                        'color':prediction_line_color
-                    }
-                ),
-                go.Line(
-                    x=timestamps,
-                    y=inflation, 
-                    name='Inflasi QoQ',
-                    mode='lines+markers'
-                )
-            ], layout= go.Layout(
-                title={
-                    'text': title,
-                    'xanchor': 'center',
-                    'x': 0.5
-                },
-                xaxis={
-                    'showgrid':False
-                },
-                yaxis={
-                    'title':{
-                        'text':title
-                    },
-                    'showgrid':False
-                },
-                font={
-                    'color': '#ccdbdc'
-                },
-                paper_bgcolor='#284e66',
-                plot_bgcolor='#284e66',
-                showlegend=True
-            )
-        ),
-        config={
-            'displayModeBar': False
-        }
-    )
 
 def plot_prediction_graph_yoy(timestamps, data, prediction, ihk, national_income, title):
 
