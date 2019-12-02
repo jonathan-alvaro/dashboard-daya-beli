87cb7c5947424bf63b8f23cf2e82cb76f8942c68
Jonathan Alvaro <jonalvarot@gmail.com>
2019-12-02 14:30:18 +0700

    Cleanup data and assets folder

--- assets/load_data.js
+++ assets/load_data.js
@@ -1,33 +1,3 @@
-function get_map_data() {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open("GET","http://localhost:4000/api",true);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-                var json_string = xhr.response;
-                resolve(json_string);
-            }
-        }
-    });
-}
-
-function get_index_data() {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open("GET","http://localhost:4000/index_data",true);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-                var json_string = xhr.response;
-                resolve(json_string);
-            }
-        }
-    });
-}
-
 function get_food_data() {
     return new Promise(function(resolve, reject) {
         var xhr = new XMLHttpRequest();
--- utils/data.py
+++ utils/data.py
@@ -1,24 +1,6 @@
 import pandas as pd
 
 
-def load_qoq_data(csv_path) -> (pd.DataFrame, pd.Series, pd.Series, pd.Series):
-    df = pd.read_csv(csv_path)
-
-    timestamps = create_timestamp(df['Tahun'], df['Quarter'])
-
-    y = df['Daya Beli']
-    inflation = df['Inflasi(QoQ)']
-    X = df.drop(
-        [
-            'Daya Beli', 'Tahun', 'Inflasi(QoQ)',
-            'Growth Pendapatan(QoQ)', 'GDP PPP'
-        ],
-        axis=1
-    ).diff().fillna(0)
-
-    return (X, y, inflation, timestamps)
-
-
 def load_yoy_data(csv_path, num_quarters=12) -> (
     pd.DataFrame, pd.Series, pd.Series, pd.Series, pd.Series
 ):
